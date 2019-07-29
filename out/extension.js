5b98ac8186f7de8644a99c2c568e9150b89a29d3
ding hao <dinghao360@163.com>
2019-07-29 16:50:29 +0800

    继续完善， 尝试加入谷歌翻译

--- out/extension.js
+++ out/extension.js
@@ -16,10 +16,19 @@ const Translate = require('../baidu_translate.js');
 // import 'google-translate-api';
 const translate = require('google-translate-api');
 var text = "flower";
+//对翻译结果进行处理
+function translateResHandler(res) {
+    let resArr = res.split(' ');
+    console.log('监视测试');
+    resArr.map(item => firstUpperCase(item));
+    return resArr.join(); //数组转字符串	
+}
+function firstUpperCase(str) {
+    return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+}
 function activate(context) {
     let provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
         provideCompletionItems(document, position, token, context) {
-            // a simple completion item which inserts `Hello World!`
             const simpleCompletion = new vscode.CompletionItem('Hello World!');
             // a completion item that inserts its text as snippet,
             // the `insertText`-property is a `SnippetString` which we will
@@ -31,7 +40,7 @@ function activate(context) {
             // the `commitCharacters`-property is set which means that the completion will
             // be inserted and then the character will be typed.
             const commitCharacterCompletion = new vscode.CompletionItem('var');
-            commitCharacterCompletion.commitCharacters = [' '];
+            // commitCharacterCompletion.commitCharacters = ['z'];p
             commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
             // a completion item that retriggers IntelliSense when being accepted,
             // the `command`-property is set which the editor will execute after 
@@ -53,6 +62,7 @@ function activate(context) {
     const provider2 = vscode.languages.registerCompletionItemProvider('plaintext', {
         provideCompletionItems(document, position) {
             return __awaiter(this, void 0, void 0, function* () {
+                // console.log(result);
                 //截取 'var ' 后的字符串  。需要翻译的文本s
                 if (position.character >= 4) {
                     //检测输入是否是汉字  先删除  正则测试方法 reg.test(str);
@@ -66,21 +76,22 @@ function activate(context) {
                     let startIndex = varIndex + 4;
                     let translateChar = document.lineAt(position).text.substr(startIndex, position.character).trim(); // 需要翻译的文字
                     //translate
-                    console.log('开始翻译。。。。');
+                    console.log('开始是翻译。。。。');
                     let baiduTranslateResults = yield Translate.baidu_translate(translateChar);
+                    baiduTranslateResults = translateResHandler(baiduTranslateResults);
                     //生成提示条目
                     let alertItems = [];
                     for (let i in baiduTranslateResults) {
                         alertItems.push(new vscode.CompletionItem(baiduTranslateResults[i], vscode.CompletionItemKind.Method));
                     }
+                    console.log('开始翻是译。。 ssss。。', alertItems);
                     return alertItems;
                     // return;
                 }
             });
         }
-    }, ' ' // triggered whenever a '.' is being typed
+    }, 'undefined' // triggered whenever a '' is being typed
     );
     context.subscriptions.push(provider1, provider2);
 }
 exports.activate = activate;
-//# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@
 import axios from 'axios';
 import * as vscode from 'vscode';
 const Translate = require('../baidu_translate.js');
-var nodejieba = require("nodejieba");
 // import 'google-translate-api';
 const translate = require('google-translate-api');
 var text = "flower";
@@ -15,13 +14,20 @@ interface axiosRes {
 	text: string;
 }
 
+//对翻译结果进行处理
+function translateResHandler(res: string): string{ 
+	let resArr = res.split(' ');
+	console.log('监视测试');
+	resArr.map( item => firstUpperCase(item) );
+	return resArr.join(); //数组转字符串	
+}
+function firstUpperCase(str: string): string {
+	return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	nodejieba.load();
 	let provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
-
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-			// a simple completion item which inserts `Hello World!`
 			const simpleCompletion = new vscode.CompletionItem('Hello World!');
 			// a completion item that inserts its text as snippet,
 			// the `insertText`-property is a `SnippetString` which we will
@@ -29,23 +35,21 @@ export function activate(context: vscode.ExtensionContext) {
 			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
 			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
 			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-
 			// a completion item that can be accepted by a commit character,
 			// the `commitCharacters`-property is set which means that the completion will
 			// be inserted and then the character will be typed.
 			const commitCharacterCompletion = new vscode.CompletionItem('var');
-			commitCharacterCompletion.commitCharacters = ['z'];
+			// commitCharacterCompletion.commitCharacters = ['z'];p
 			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-
 			// a completion item that retriggers IntelliSense when being accepted,
 			// the `command`-property is set which the editor will execute after 
 			// completion has been inserted. Also, the `insertText` is set so that 
 			// a space is inserted after `new`
+			
 			const commandCompletion = new vscode.CompletionItem('new');
 			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
 			commandCompletion.insertText = 'new ';
 			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-
 			// return all completion items as array
 			return [
 				simpleCompletion,
@@ -56,13 +60,9 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	const provider2 = vscode.languages.registerCompletionItemProvider(
-		'plaintext',
-		{
+	const provider2 = vscode.languages.registerCompletionItemProvider('plaintext',{
 		async 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-			var result =  await nodejieba.cut("获得对应的列表位置元素");
-			console.log(result);
+			// console.log(result);
 				//截取 'var ' 后的字符串  。需要翻译的文本s
 				if (position.character >= 4) { 
 						//检测输入是否是汉字  先删除  正则测试方法 reg.test(str);
@@ -77,21 +77,23 @@ export function activate(context: vscode.ExtensionContext) {
 					let startIndex = varIndex + 4;
 					let translateChar = document.lineAt(position).text.substr( startIndex, position.character).trim(); // 需要翻译的文字
 						//translate
-					console.log('开始翻译。。。。');
-					let  baiduTranslateResults  =   await Translate.baidu_translate(translateChar);
+					console.log('开始是翻译。。。。');
+					let baiduTranslateResults = await Translate.baidu_translate(translateChar);
+					baiduTranslateResults = translateResHandler(baiduTranslateResults);
 					//生成提示条目
 					let alertItems = [];
 					for (let i in  baiduTranslateResults) { 
 						alertItems.push( new vscode.CompletionItem( baiduTranslateResults[i], vscode.CompletionItemKind.Method) );
 					}	
-				
+					console.log('开始翻是译。。 ssss。。', alertItems);
+
 					return alertItems;
 					// return;
 				}
 
 			}
 		},
-		'z' // triggered whenever a '' is being typed
+		'undefined' // triggered whenever a '' is being typed
 	);
 
 	context.subscriptions.push(provider1, provider2);
